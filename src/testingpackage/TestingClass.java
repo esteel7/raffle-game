1cfc24d1f355c800f79eccb6ca883aa4a6fd070a
Rodrigo Martínez <rodrigodr7@gmail.com>
2019-02-27 18:40:41 -0300

    Added Field variables, setters and getters in TestingClass

--- src/testingpackage/TestingClass.java
+++ src/testingpackage/TestingClass.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testingpackage;
 
 /**
  *
- * @author Rodrigo
+ * @author Rodrigo Martínez
  */
 public class TestingClass {
-    
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }
