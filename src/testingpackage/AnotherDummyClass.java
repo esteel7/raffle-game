4b07f6ae9ae4ecbfb788dd7fa7033a6547ef7e2c
Rodrigo Martínez <rodrigodr7@gmail.com>
2019-02-27 18:49:38 -0300

    Added AnotherDummyClass

---
+++ src/testingpackage/AnotherDummyClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingpackage;
+
+/**
+ *
+ * @author Rodrigo Martínez
+ */
+public class AnotherDummyClass {
+    
+}
